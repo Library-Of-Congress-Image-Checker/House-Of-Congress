471eb43dd3ab05e8a78e23ee1595a61afe48145e
Sean Campbell <campbellsean211@gmail.com>
2019-12-04 17:57:40 -0500

    Cookies Implemented

--- Image_Checker/src/App.js
+++ Image_Checker/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar.js';
 import ImageList from './ImageList.js';
+import cookie from 'react-cookies';
 import axios from 'axios';
 import scrollToComponent from 'react-scroll-to-component';
 import './body.css';
@@ -10,14 +11,27 @@ class App extends React.Component {
 state = { Image: [], isLoading: false};
 
   onSearchSubmit=async term => {
-    const response = await axios.get("https://api.unsplash.com/search/photos/?client_id=f32d02ca2ffc63e6c39b8c66bfec0bd3cd6841995c0e1e695d1478f1babeb730",
-      {
-          params: {
-          query: term,
-          per_page: 30,
-      },
-        headers: { Authorization: 'Client-ID f32d02ca2ffc63e6c39b8c66bfec0bd3cd6841995c0e1e695d1478f1babeb730'}
-    });
+
+    var url = "https://api.unsplash.com/search/photos/?client_id=f32d02ca2ffc63e6c39b8c66bfec0bd3cd6841995c0e1e695d1478f1babeb730";
+    url += "&query=" + term;
+    let response = undefined;
+
+    if(cookie.load(term) == url){
+      console.log("cookie found");
+      console.log(cookie.load(term));
+      response = await axios.get(cookie.load(term));
+    }else {
+      console.log("no cookie");
+      cookie.save(term, url);
+      response = await axios.get("https://api.unsplash.com/search/photos/?client_id=f32d02ca2ffc63e6c39b8c66bfec0bd3cd6841995c0e1e695d1478f1babeb730",
+       {
+           params: {
+           query: term,
+           per_page: 30,
+       },
+         headers: { Authorization: 'Client-ID f32d02ca2ffc63e6c39b8c66bfec0bd3cd6841995c0e1e695d1478f1babeb730'}
+     });
+   }
 
     this.setState({Image: response.data})
     scrollToComponent(this.Indigo, { offset: 0, align: 'top', duration: 50, ease:'inExpo'})
@@ -57,6 +71,7 @@ isLoadingCondition() {
   render() {
 
       console.log(this.state.isLoading)
+      console.log(cookie.load('car'));
 
       return (
         <>
